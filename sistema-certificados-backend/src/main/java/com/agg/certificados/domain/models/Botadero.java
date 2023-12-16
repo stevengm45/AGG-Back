93bf2a02c95108c3f1af3cc1232c718a26e2eedd
dmedina03 <dmedina@soaint.com>
2023-12-16 12:56:31 -0500

    Creacion de entidad botadero

---
+++ sistema-certificados-backend/src/main/java/com/agg/certificados/domain/models/Botadero.java
@@ -0,0 +1,10 @@
+package com.agg.certificados.domain.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "botadero")
+public class Botadero {
+
+}
