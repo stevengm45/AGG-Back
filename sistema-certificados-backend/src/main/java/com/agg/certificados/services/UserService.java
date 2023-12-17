af2712141ae69c941831414748241ab3c9b6ec71
stevengm45 <stevengm45@gmail.com>
2023-12-17 16:13:43 -0500

    se ajustan cambios en service y controller

--- sistema-certificados-backend/src/main/java/com/agg/certificados/services/UserService.java
+++ sistema-certificados-backend/src/main/java/com/agg/certificados/services/UserService.java
@@ -1,4 +1,4 @@
-package services;
+package com.agg.certificados.services;
 
 import com.agg.certificados.entity.User;
 import com.agg.certificados.entity.UserRol;
@@ -7,8 +7,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
-@Service
 public interface UserService {
 
     public User saveUser(User user, Set<UserRol> userRoles) throws Exception;
+
+    public User getUser(String username);
+
+    public void deleteUser(Long userId);
 }
