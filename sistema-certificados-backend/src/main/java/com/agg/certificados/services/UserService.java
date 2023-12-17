1a1fce53749e7da300a78184b06e5a43d53af6f3
stevengm45 <stevengm45@gmail.com>
2023-12-17 16:00:01 -0500

    se agrega controller y se ajusta error por sevices.UserSevice

--- sistema-certificados-backend/src/main/java/com/agg/certificados/services/UserService.java
+++ sistema-certificados-backend/src/main/java/com/agg/certificados/services/UserService.java
@@ -2,9 +2,12 @@ package services;
 
 import com.agg.certificados.entity.User;
 import com.agg.certificados.entity.UserRol;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public interface UserService {
 
     public User saveUser(User user, Set<UserRol> userRoles) throws Exception;
