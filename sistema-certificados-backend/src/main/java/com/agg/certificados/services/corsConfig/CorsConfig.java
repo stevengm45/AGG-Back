4adc1af80f45be38aafcf741d3240a0ab8bfcc58
dmedina03 <dmedina@soaint.com>
2024-01-30 20:42:31 -0500

    Se sube a github

--- sistema-certificados-backend/src/main/java/com/agg/certificados/services/corsConfig/CorsConfig.java
+++ sistema-certificados-backend/src/main/java/com/agg/certificados/services/corsConfig/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200") // Reemplaza con la URL de tu aplicación Angular
+                        .allowedOrigins("http://localhost:4200","https://suministramosycontratamosagg.com") // Reemplaza con la URL de tu aplicación Angular
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
